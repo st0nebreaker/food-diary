37e655983cc6086a66d5d84b5577192dde858c80
Becca Steinbrecher <beccasteinbrecher@gmail.com>
2020-07-29 18:53:11 -0600

    date picker for entry form

--- src/App/App.js
+++ src/App/App.js
@@ -18,16 +18,18 @@ class App extends Component {
 		return (
 			<div className='App'>
 				<Header />
-				<LogHistory />
-				<EntryForm />
-				<Route 
-					path='/trends' 
-					render={() => (
-						<Trends />
-					)}
-				/>
-				{/* Route to LogHistoryDetails */}
-				{/* Route to FoodDetails */}
+				<section className='layout-body'>
+					<LogHistory />
+					<EntryForm />
+					<Route 
+						path='/trends' 
+						render={() => (
+							<Trends />
+						)}
+					/>
+					{/* Route to LogHistoryDetails */}
+					{/* Route to FoodDetails */}
+				</section>
 			</div>
 		);
 	}
--- src/EntryForm/EntryForm.js
+++ src/EntryForm/EntryForm.js
@@ -1,13 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
+import DatePicker from "react-datepicker";
 import { Link } from "react-router-dom";
 import "./EntryForm.scss";
+import "react-datepicker/dist/react-datepicker.css";
 
-const EntryForm = () => {
-	return (
-		<>
-			<section>ENTRYFORM!</section>
-		</>
-	)
+
+class EntryForm extends Component {
+	constructor() {
+		super();
+		this.state = {
+			date: new Date()
+		}
+	}
+
+	handleChange = date => {
+    this.setState({
+			date: date,
+			searchInput: 'food search...'
+    });
+  };
+
+	render() {
+		return (
+			<form className='entry-form'>
+				<section>New Food Entry</section>
+				<DatePicker
+					selected={this.state.date}
+					// onSelect={this.handleSelect} //when day is clicked
+					onChange={this.handleChange} //only when value has changed
+				/>
+				<div><input
+					type="text"
+					name="search"
+					placeholder="search cocktails..."
+					className="header-search"
+					value={this.state.searchInput}
+					// onChange={(e) => setSearchInput(e.target.value)}
+					aria-label="search"
+				/>
+				<button className='search-btn'>
+					search
+				</button></div>
+			</form>
+		)
+	}
 }
 
 export default EntryForm;
